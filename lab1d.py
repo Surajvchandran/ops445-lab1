dbf8be9aaa75779dcbc34292b7bda7bb693a6257
Suraj-hyper <surajchandran2004@gmail.com>
2025-01-24 12:40:41 -0500

    Added lab1d file

---
+++ lab1d.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Defining the objects
+x = 10
+y = 2
+z = 5
+
+# Performing the calculation and printing the result
+result = x + y * z  # Using PEMDAS: multiplication happens before addition
+print(f"{x} + {y} * {z} = {result}")
